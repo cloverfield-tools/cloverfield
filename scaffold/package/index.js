653ee66a4245c78eff057d6caf8a38d354e1e2d9
Nik Butenko <nik@butenko.me>
2015-07-04 23:06:28 +1000

    CLI framework

---
+++ scaffold/package/index.js
@@ -0,0 +1,36 @@
+#! /usr/bin/env ./node_modules/.bin/babel-node
+
+
+import prompt from 'prompt';
+import {parse} from 'nomnom';
+
+
+const schema = {
+  properties: {
+    name: {
+      description: 'Package name: ',
+      pattern: /^[a-z]+[a-z\-_]+$/,
+      message: 'Name must be only letters, numbers, dashes and underscores',
+      required: true
+    }
+  }
+};
+prompt.message = '>'.green;
+prompt.delimiter = ' ';
+prompt.colors = false;
+prompt.start();
+
+
+const scaffold = () => {
+  prompt.get(schema, (err, {name}) =>
+    console.log(`Scaffolding ${name}...`));
+};
+
+
+// Check if script is run directly
+if (require.main === module) {
+  scaffold(parse())
+}
+
+
+export default scaffold;
