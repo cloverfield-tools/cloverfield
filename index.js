e603d722f3118b67d83e99885fafa3ff1d236765
Nik Butenko <nik@butenko.me>
2015-07-06 22:13:24 +1000

    Auto-scan for globally installed scaffolds

--- index.js
+++ index.js
@@ -2,35 +2,29 @@
 
 
 import nomnom from 'nomnom';
-import glob from 'glob';
-import path from 'path';
+import findScaffolds from './lib/find-scaffolds';
 
 
-// Find all available scaffolds
-const scaffolds = {};
+findScaffolds()
+  .then(scaffolds => {
+    // `cf build <scaffold>` command
+    nomnom.command('build')
+      .option('scaffold', {
+        position: 1,
+        help: 'Scaffold name',
+        type: 'string',
+        required: true,
+        // Allow to enter only one of existing scaffolds
+        choices: Object.keys(scaffolds)
+      })
+      .callback(({scaffold, ...opts}) =>
+        require(scaffolds[scaffold])(opts))
+      .help('Build a new package from a Cloverfield scaffold');
 
-glob.sync('scaffold/*/', {realpath: true})
-  .forEach(pkg => scaffolds[path.basename(pkg)] = pkg);
+    // Set default script name to `cf` for interactive help
+    nomnom.script('cf');
 
-
-// `cf build <scaffold>` command
-nomnom.command('build')
-  .option('scaffold', {
-    position: 1,
-    help: 'Scaffold name',
-    type: 'string',
-    required: true,
-    // Allow to enter only one of existing scaffolds
-    choices: Object.keys(scaffolds)
+    // Parse input
+    nomnom.nom();
   })
-  .callback(({scaffold, ...opts}) =>
-    require(scaffolds[scaffold])(opts))
-  .help('Build a new package from a Cloverfield scaffold');
-
-
-// Set default script name to `cf` for interactive help
-nomnom.script('cf');
-
-
-// Parse input
-nomnom.nom();
+  .catch(error => console.error(error));
