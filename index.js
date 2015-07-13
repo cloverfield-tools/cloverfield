76e61a2577164147d33def12cf0dc7791cf86571
Nik Butenko <nik@butenko.me>
2015-07-13 21:39:32 +1000

    Refactor commands

--- index.js
+++ index.js
@@ -1,13 +1,19 @@
 #! /usr/bin/env ./node_modules/.bin/babel-node
 
 
+import nomnom from 'nomnom';
 import cloverfield from './lib/cloverfield';
+import findScaffolds from './lib/find-scaffolds';
+import buildCommand from './lib/commands/build';
 
 
-// Check if script is run directly
-if (require.main === module) {
-  cloverfield();
-}
+const parser = nomnom();
 
 
-export default cloverfield;
+Promise.all([
+  findScaffolds()
+    .catch(console.error.bind(console))
+    .then(buildCommand)
+])
+  .then(cloverfield(parser))
+  .then(() => parser.parse());
